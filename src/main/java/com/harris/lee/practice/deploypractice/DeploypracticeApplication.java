a24ab32225a9e176017b2641d33156c0c0e13db2
harris <harrisleesh@gmail.com>
2020-10-11 17:15:25 +0900

    first commit

---
+++ src/main/java/com/harris/lee/practice/deploypractice/DeploypracticeApplication.java
@@ -0,0 +1,13 @@
+package com.harris.lee.practice.deploypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploypracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploypracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harris/lee/practice/deploypractice/DeploypracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harris.lee.practice.deploypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploypracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
